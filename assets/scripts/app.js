972a5936e890ddbfd1aa7bcbfc8393e0cff65133
acarlotto <acarlotto@cox.net>
2017-08-20 09:26:30 -0400

    my first commit

---
+++ assets/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict'
+
+const app = {
+  host: 'http://localhost:4741',
+  user: {
+    id: null,
+    email: null,
+    token: null
+  }
+}
+
+module.exports = app
---
+++ assets/scripts/config.js
@@ -0,0 +1,9 @@
+'use strict'
+
+const config = {
+  apiOrigins: {
+    production: 'https://localhost:4741'
+  }
+}
+
+module.exports = config
